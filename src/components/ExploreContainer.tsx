fffc04e65c348e00784daca5134ba87432598edb
Chris Wessels <chrisw@nashuacapetown.co.za>
2023-06-30 09:20:22 +0200

    First commit

---
+++ src/components/ExploreContainer.tsx
@@ -0,0 +1,14 @@
+import './ExploreContainer.css';
+
+interface ContainerProps { }
+
+const ExploreContainer: React.FC<ContainerProps> = () => {
+  return (
+    <div className="container">
+      <strong>Ionic One Home</strong>
+      <p>More information on <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+    </div>
+  );
+};
+
+export default ExploreContainer;
